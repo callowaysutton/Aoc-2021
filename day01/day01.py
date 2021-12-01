4143c294feb34827fa7a2321141f50217ebad305
callowaysutton <calloway@myself.com>
2021-12-01 00:35:10 -0500

    Cleanup

--- day01/day01.py
+++ day01/day01.py
@@ -1,22 +1,41 @@
 scan = []
-prev = -float('inf')
-count = 0
-
 with open("input.txt") as lines:
     for line in lines:
         scan.append(int(line.strip()))
 
-for i,pulse in enumerate(scan):
-    a = 0
-    try:
-        for j in range(3):
-            a+=scan[i+j]
-    except:
-        break
-    if a > prev:
-        prev = a
-        count+=1
-    else:
-        prev = a
+def p1(scan):
+    """Part One Solution
+    """
+    prev = -float('inf')
+    count = 0
+    for i,pulse in enumerate(scan):
+        if pulse > prev:
+            prev = pulse
+            count+=1
+        else:
+            prev = pulse
 
-print(count-1)
+    return count-1
+
+def p2(scan):
+    """Part Two Solution
+    """
+    prev = -float('inf')
+    count = 0
+    for i,pulse in enumerate(scan):
+        a = 0
+        try:
+            for j in range(3):
+                a+=scan[i+j]
+        except:
+            break
+        if a > prev:
+            prev = a
+            count+=1
+        else:
+            prev = a
+
+    return count-1
+
+print(f"Problem 1 Solution: {p1(scan)}")
+print(f"Problem 2 Solution: {p2(scan)}") 
